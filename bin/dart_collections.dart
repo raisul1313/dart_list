fdef479a88d73a630cdc1579682c4c374ee348e6
raisul1313 <raisul.islam1313@gmail.com>
2022-03-22 15:19:00 +0600

    first commit

---
+++ bin/dart_collections.dart
@@ -0,0 +1,15 @@
+void main() {
+  var list = [12, 14, 16, 20, 24, 45, 59, 35, 89, 55];
+
+  print(list.length);
+  print(list[list.length -1]);
+  print(list[4]);
+
+  for (int i = 0; i < list.length; i++) {
+    if (list[i] % 2 == 0) {
+      print("Index $i contains ${list[i]} can multiple by 2");
+    } else {
+      print("Index $i contains ${list[i]} can't multiple by 2");
+    }
+  }
+}
